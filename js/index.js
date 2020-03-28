b0b0cbc58b527aee2b8f01f9e2d09f48aa373b6d
Hendy <hendy.almeida@hotmail.com>
2020-03-28 00:06:33 -0300

    fix request

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 $(function (){
     $.ajax({
         type: 'GET',
-        url: 'https://127.0.0.0:8000/devs',
+        url: 'http://localhost:8000/api/devs',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
+        secure: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        cors: true,
         success: function(devs) {
-            console.log('deu bom', devs);
-            $.each(devs, function(i, dev){
-                $devs.append(`<p class="card-text">${dev.nome}</p>
+            console.log('deu bom');
+            for (let dev of devs){
+                $('#devs').append(`<p class="card-text">${dev.name}</p>
                 <p class="card-text">${dev.email}</p>
                 <p class="card-text">${dev.level}</p>
-                <li class="list-group-item">${dev.skill}</li>`);
-            })
+                <li class="list-group-item">${dev.skill ? devskill : 'nenhuma skill cadastrada'}</li>`);
+            }
 
         }
     });
